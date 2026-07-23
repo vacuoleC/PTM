9dad0f5546375ed2862fef661eec775717b9c996
vacuolec <2947435737@qq.com>
2026-07-24 04:12:37 +0800

    Configure clinical focus script from YAML

--- scr/focus.py
+++ scr/focus.py
@@ -1,8 +1,15 @@
 import cptac
-for name, cls in [("LUAD", cptac.Luad), ("UCEC", cptac.Ucec)]:
+from cptac_setup import configure_cptac
+from project_config import CONFIG, get_cohort_class
+
+
+configure_cptac()
+
+for name in CONFIG["datasets"]["focus_cohorts"]:
     print("=" * 60, name)
-    c = cls().get_clinical("mssm")
-    for col in ["histologic_grade", "tumor_stage_pathological"]:
+    cohort = get_cohort_class(cptac, name)()
+    c = cohort.get_clinical(CONFIG["cptac"]["clinical_source"])
+    for col in CONFIG["clinical"]["focus_columns"]:
         if col in c.columns:
             print(f"\n[{col}]")
             print(c[col].value_counts(dropna=False))
