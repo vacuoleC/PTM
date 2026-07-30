87cf800fd3ab904822fa49ca8bdce4597f3ec04a
vacuolec <2947435737@qq.com>
2026-07-30 17:47:01 +0800

    Add PTM fold scoring primitive

---
+++ PTMv2/scr/evaluate.py
@@ -0,0 +1,9 @@
+"""Fold-safe primitive for PTMv2 model evaluation."""
+from sklearn.linear_model import LogisticRegression
+from preprocessing import make_preprocessing_pipeline
+
+def fit_score_fold(X_train,y_train,X_test,threshold,C,l1_ratio):
+    prep=make_preprocessing_pipeline(threshold)
+    Xt=prep.fit_transform(X_train); Xv=prep.transform(X_test)
+    model=LogisticRegression(penalty='elasticnet',solver='saga',C=C,l1_ratio=l1_ratio,max_iter=10000,random_state=0)
+    return model.fit(Xt,y_train).predict_proba(Xv)[:,1]
